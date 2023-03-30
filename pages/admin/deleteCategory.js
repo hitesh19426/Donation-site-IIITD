4a6706532330f268f8ce39680c50a8478c963c88
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-03-30 20:20:58 +0530

    added pages for add, edit and delete category

--- pages/admin/deleteCategory.js
+++ pages/admin/deleteCategory.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { useSession, getSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
 import Link from "next/link";
 
-const AdminPage = ({session}) => {
+const DeleteCategoryPage = ({session}) => {
   return (
     <div className="d-grid gap-4 d-md-flex">
-      <Link href={`/admin/addCategory`} className="btn btn-success">
-        Add New Category
-      </Link>
-      <Link href={`/admin/editCategory`} className="btn btn-success">
-        Edit Category
-      </Link>
-      <Link href={`/admin/deleteCategory`} className="btn btn-success">
-        Delete Category
-      </Link>
+      DeleteCategoryPage
     </div>
   );
 };
@@ -38,4 +30,4 @@ export async function getServerSideProps({req}) {
   };
 }
 
-export default AdminPage;
+export default DeleteCategoryPage;
---
+++ pages/admin/editCategory.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { getSession } from "next-auth/react";
+import Link from "next/link";
+import { Form, Formik } from "formik";
+import MyTextInput from "@/components/MyTextInput";
+
+const initialValues = {
+    name: "",
+    description: "",
+  };
+
+  const validator = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Required";
+    }
+    if (!values.description) {
+      errors.description = "Required";
+    }
+    return errors;
+  };
+
+const EditCategoryPage = ({session}) => {
+    const handleSubmit = async (values) => {
+        console.log("handle submit function called");
+        console.log(values);
+      };
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <Formik
+          initialValues={initialValues}
+          validate={validator}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <MyTextInput
+              label="Category Name"
+              name="name"
+              type="text"
+              placeholder="Give for Alumni Legacy"
+            />
+
+            <MyTextInput
+              label="Description"
+              name="description"
+              type="text"
+              placeholder="Enter Description of the category"
+            />
+            
+            {/* TODO: If you have time, then disable button if there are errors */}
+            <div className="col-12 ms-5 mt-3">
+              <button type="submit" className="btn btn-success">
+                Submit
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+export async function getServerSideProps({req}) {
+  const session = await getSession({ req });
+  console.log("session in admin page = ", session);
+
+  if (!session) {
+    return { 
+      redirect: {
+        destination: "/api/auth/signin",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
+
+export default EditCategoryPage;
