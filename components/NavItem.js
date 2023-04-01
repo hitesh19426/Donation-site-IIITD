b1bb793f0f77bcca5be882cafef8b3558a1c3383
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-04-01 12:27:12 +0530

    added addCategory page

--- components/NavItem.js
+++ components/NavItem.js
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
-export default function NavItem({item, path}) {
+export default function NavItem({key, item, path}) {
   return (
-    <li className="nav-item">
+    <li className="nav-item" key={key}>
       <Link href={`/${path}/${item.id}`} className="nav-link">
         <span className="text-dark">{item.name}</span>
       </Link>
--- pages/admin/addCategory.js
+++ pages/admin/addCategory.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { Form, Formik } from "formik";
 import MyTextInput from "@/components/MyTextInput";
 import Image from "next/image";
+import { server } from "@/config";
+
 
 const initialValues = {
   name: "",
@@ -23,12 +25,43 @@ const validator = (values) => {
 
 const AddCategoryPage = ({ session }) => {
   const [uploading, setUploading] = useState(false);
-  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedFile, setSelectedFile] = useState();
 
   const handleSubmit = async (values) => {
     console.log("handle submit function called");
     console.log(values);
+
+    console.log(values.name);
+    console.log(values.description);
+
+    const form = new FormData();
+    form.append('name', values.name);
+    form.append('description', values.description);
+    form.append('imageUrl', selectedFile);
+    console.log(form);
+
+    const result = await fetch(`${server}/api/admin/addCategory`, {
+      method: 'POST',
+      body: form,
+    });
+    const res = await result.json();
+
+    console.log(res);
   };
+
+  const handleImage = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file)
+    console.log(file);
+
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      console.log(event.target.result);
+      setSelectedImage(event.target.result);
+    }
+    fileReader.readAsDataURL(file)
+  }
 
   return (
     <div className="card">
@@ -52,6 +85,34 @@ const AddCategoryPage = ({ session }) => {
               type="text"
               placeholder="Enter Description of the category"
             />
+
+            {/* {console.log("selectedImage = ", selectedImage)} */}
+
+            <div>
+              <input type="file" className="" onChange={handleImage} />
+
+              {selectedImage && <Image src={selectedImage} alt="" width={300} height={300} />}
+            
+            </div>
+            {/* <label>
+              <input type="file" hidden onChange={ ({target}) => {
+                if(target.files){
+                  const file = target.files[0];
+                  setSelectedImage(URL.createObjectURL(file));
+                }
+              }}/>
+              <div>
+                {selectedImage ? (
+                  <Image href={selectedImage} alt="" className="card-img-top" width={500} height={300} />
+                ) : (
+                  <span> Select Image </span>
+                )}
+              </div>
+            </label> */}
+
+            {/* <button disabled={uploading} className="btn btn-success">
+                {uploading ? "Uploading..." : "Upload"}
+            </button> */}
 
             {/* TODO: If you have time, then disable button if there are errors */}
             <div className="col-12 ms-5 mt-3">
