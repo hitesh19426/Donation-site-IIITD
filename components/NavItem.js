2cab6767dcf14c5089b03ec737ce32f0d00764ec
V15hnu24 <799vishnu530@gmail.com>
2023-04-07 14:09:18 +0530

    Commit of main homepage update

--- components/NavItem.js
+++ components/NavItem.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavItem({item, path}) {
   return (
-    <li className="nav-item" key={item.id}>
+    <li className="nav-item category" key={item.id}>
       <Link href={`/${path}/${item.id}`} className="nav-link">
         <span className="text-dark">{item.name}</span>
       </Link>
