4f8465e63d923e68daf5ca8a8083f25497d21c41
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-04-08 15:04:42 +0530

    fixed footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -7,59 +9,31 @@ export default function Footer() {
       style={{backgroundColor: "#ECEFF1"}}
     >
       <section
-        className="d-flex justify-content-between p-4 text-white"
+        className="d-flex justify-content-start p-4 text-white"
         style={{backgroundColor: "#21D192"}}
       >
-        <div className="me-5">
+        <div className="me-3">
           <span>Get connected with us on social networks:</span>
         </div>
 
         <div>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-facebook-f"></i>
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-google"></i>
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-github"></i>
-          </a>
-        </div>
-      </section>
-
-      <section className="">
-        <div className="container text-center text-md-start mt-5">
-          <div className="row mt-3">
-            <div className="col-md-3 col-lg-4 col-xl-3 mx-2 mb-4">
-              <h6 className="text-uppercase fw-bold">Company name</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
-              />
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </div>
-          </div>
+          <Link href="#" className="text-white me-4">
+            <FaFacebookF />
+          </Link>
+          <Link href="#" className="text-white me-4">
+            <FaTwitter />
+          </Link>
+          <Link href="#" className="text-white me-4">
+          <FaInstagram />
+          </Link>
+          <Link href="#" className="text-white me-4">
+            <FaLinkedin />
+          </Link>
         </div>
       </section>
 
       <div className="text-center p-3" style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
-        © 2020 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
+        © 2020 Copyright: IIIT-Delhi. All rights reserved.
       </div>
     </footer>
   );
