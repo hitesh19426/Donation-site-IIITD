1157f743e664be62e2cff04ff442dde883b366fd
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-05-02 15:25:40 +0530

    made sidebar link go to payment directly

--- components/NavItem.js
+++ components/NavItem.js
@@ -4,7 +4,7 @@ export default function NavItem({item, path}) {
   return (
     <li className="nav-item category "  key={item.id}>
       {/* //add hover effect to the nav item */}
-      <Link href={`/${path}/${item.id}`} className="nav-link"  >
+      <Link href={`/${path}/${item.id}/payment`} className="nav-link"  >
         <span className="text-dark" >{item.name}</span>
       </Link>
     </li>
