cdb246e13af9398b24d0fcb26fa767b64a466798
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-05-02 15:04:52 +0530

    removed multer in add/edit category

--- pages/api/admin/addCategory.js
+++ pages/api/admin/addCategory.js
@@ -1,11 +1,9 @@
 import dbConnect from "@/utils/dbConnect";
 import category from "@/models/category";
-import multer from "multer";
 import { createRouter } from "next-connect";
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/pages/api/auth/[...nextauth]"
-
-import { Uplaod } from "@/s3";
+import { Upload } from "@/s3";
 
 dbConnect();
 
@@ -15,30 +13,19 @@ export const config = {
   },
 };
 
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: './public/uploads',
-    filename: function (req, file, callback) {
-      callback(null, new Date().getTime() + "-" + file.originalname);
-    },
-  }),
-});
-
 const router = createRouter();
 
-router.use(upload.single("imageUrl")).post(async (req, res, next) => {
+router.post(async (req, res, next) => {
   const session = await getServerSession(req, res, authOptions);
   if(!session)
     return res.status(401).json({ success: false, message: "Unauthorized" })
   
-  const { method } = req;
-
   console.log("file = ", req.file);
   console.log("body = ", req.body);
 
   console.log("file name:" + req.file.filename);
 
-  const s3Upload = await Uplaod(req.file);
+  const s3Upload = await Upload(req.file);
 
   console.log("s3Upload = ", s3Upload);
 
@@ -54,8 +41,8 @@ router.use(upload.single("imageUrl")).post(async (req, res, next) => {
   } catch (error) {
     return res.status(400).json({ success: false, message: error.message });
   }
+
   
-  //   return res.status(201).json({ body: req.body, file: req.file });
 });
 
 export default router.handler({
